4ff941ea29483b030ffdb863ebbf2e6e9b79870b
Odukogbe Adewale <oduswale1@gmail.com>
2025-11-11 17:10:29 +0100

    modify env in frnt end

--- giftlink-frontend/src/components/Navbar/Navbar.js
+++ giftlink-frontend/src/components/Navbar/Navbar.js
@@ -3,11 +3,30 @@ import React from 'react';
 export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            {/* Brand Logo / Name */}
             <a className="navbar-brand" href="/">GiftLink</a>
 
+            <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarNav"
+                aria-controls="navbarNav"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
             <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    {/* Task 1: Add links to Home and Gifts below*/}
+                <ul className="navbar-nav ml-auto">
+                    {/* Task 1: Add links to Home and Gifts */}
+                    <li className="nav-item">
+                        <a className="nav-link" href="/home.html">Home</a> {/* Link to home page */}
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="/app">Gifts</a> {/* Link to MainPage */}
+                    </li>
                 </ul>
             </div>
         </nav>
